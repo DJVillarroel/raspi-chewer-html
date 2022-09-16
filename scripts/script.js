477f82683707211e2f2bca73cf42ed83dfa87542
Diego Villarroel <thedjv123@gmail.com>
2022-09-16 20:59:22 -0300

    pullea lpm

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 console.log('Messi');
 
 
-var w = 1000;
-var h = 600;
+var w = 2500;
+var h = 800;
 var columns;
 var rows;
-var scl = 20;
+var scl = 70;
 
 
 function setup(){
 	columns = w / scl;
 	rows = h / scl;
-	createCanvas(windowWidth, 800, WEBGL);
+	createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw(){
 	background(4, 26, 37);
-	rotateX(PI/3);
+	rotateX(PI/2.5);
 	translate(-w/2,-h/2);
 	for (let i = 0; i < columns; i++) {
 		for (let j = 0; j < rows; j++) {
